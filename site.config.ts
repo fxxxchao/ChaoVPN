07dc770fb9c1ef3be1878292f0d2a8de58696c1a
Chao <fxxxchao@gmail.com>
2023-01-05 22:40:47 +0800

    更新博客信息

--- site.config.ts
+++ site.config.ts
@@ -11,17 +11,17 @@ export default siteConfig({
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'fxxxchao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: 'fxxxchao',
+  youtube: 'fxxxchao', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
   github: 'fxxxchao',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
